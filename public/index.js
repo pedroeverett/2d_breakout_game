cedd685e8a086024388ff2fe4d7c8eb0b92db2dc
Pedro Everett <pedrojoseeverett@gmail.com>
2017-08-03 19:42:49 +0100

    index.js drawPaddle()

--- public/index.js
+++ public/index.js
@@ -5,6 +5,9 @@ window.addEventListener('load', function() {
   var context = canvas.getContext('2d');
  
   var ballSize = 10;
+  var paddleHeight = 10;
+  var paddleWeidth = 75;
+  var paddleX = (canvas.width - paddleWeidth) / 2;   
   var x = canvas.width / 2;
   var y = canvas.height - 30;
   var mx = 2;
@@ -14,6 +17,14 @@ window.addEventListener('load', function() {
     context.beginPath();
     context.arc(x, y, ballSize, 0, Math.PI * 2);
     context.fillStyle = 'orange';
+    context.fill();
+    context.closePath();
+  }
+
+  var drawPaddle = function() {
+    context.beginPath();
+    context.rect(paddleX, canvas.height - paddleHeight, paddleWeidth, paddleHeight);
+    context.fillStyle = 'black';
     context.fill();
     context.closePath();
   }
@@ -28,10 +39,10 @@ window.addEventListener('load', function() {
       my = -my;
     }
 
-
     if (x + mx < ballSize || x + mx > canvas.width - ballSize) {
       mx = -mx;
     }
+    drawPaddle();
   }
   setInterval(drawObjects, 10);
 
