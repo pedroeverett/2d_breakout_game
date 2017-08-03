7ce6443aa536ea9d683877a3eeb11177a3cd7c25
Pedro Everett <pedrojoseeverett@gmail.com>
2017-08-03 19:14:29 +0100

    index.js initial drawBall()

--- public/index.js
+++ public/index.js
@@ -4,11 +4,26 @@ window.addEventListener('load', function() {
   var canvas = document.getElementById('gameCanvas');
   var context = canvas.getContext('2d');
 
-  //text draw on canvas
-  context.beginPath();
-  context.rect(60,40,30,30);
-  context.fillStyle = 'orange';
-  context.fill();
-  context.closePath();  
+  // //text draw on canvas
+  // context.beginPath();
+  // context.rect(60,40,30,30);
+  // context.fillStyle = 'orange';
+  // context.fill();
+  // context.closePath();  
+  var x = canvas.width / 2;
+  var y = canvas.height - 30;
+  var mx = 2;
+  var my = -2;
+
+  var drawBall = function() {
+    context.beginPath();
+    context.arc(x, y, 10, 0, Math.PI * 2);
+    context.fillStyle = 'orange';
+    context.fill();
+    context.closePath();
+    x += mx;
+    y += my;
+  }
+  setInterval(drawBall, 10);
 
 })
