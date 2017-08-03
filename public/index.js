5a43dbab16e62ea908d3eb0c4a875fdf6db225ec
Pedro Everett <pedrojoseeverett@gmail.com>
2017-08-03 18:58:03 +0100

    index.js test can draw on canvas

--- public/index.js
+++ public/index.js
@@ -1,6 +1,14 @@
 window.addEventListener('load', function() {
 
-  var canvas = document.querySelector('#gameCanvas');
+  //var canvas = document.querySelector('.gameCanvas');
+  var canvas = document.getElementById('gameCanvas');
   var context = canvas.getContext('2d');
+
+  //text draw on canvas
+  context.beginPath();
+  context.rect(60,40,30,30);
+  context.fillStyle = 'orange';
+  context.fill();
+  context.closePath();  
 
 })
